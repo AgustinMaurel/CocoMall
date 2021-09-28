1a79c815f7a56309d93d4b9a4b0010bc66583cc6
AgustinMaurel <85203984+AgustinMaurel@users.noreply.github.com>
2021-09-28 09:57:58 -0300

    last change

--- coco-mall/src/App.js
+++ coco-mall/src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
 
 function App() {
-    return (
-        <div className="App">
-            <h1 className="bg-red-500">BIENVENUTE A COCOmall</h1>
-            
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1 className="bg-red-500">BIENVENUTE A COCO mall</h1>
+    </div>
+  );
 }
 
 export default App;

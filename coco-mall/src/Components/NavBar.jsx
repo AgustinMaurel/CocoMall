acf77129edb6c29f1e33342b292a881f4dfcef13
MatiMonas <matutin97@gmail.com>
2021-09-28 13:42:10 -0300

    Creates nav component

---
+++ coco-mall/src/Components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function NavBar(props) {
+
+    //La Nav va a recibir props porque vamos a renderizar distintas cosas dependiendo
+    //el tamaño de la pantalla.
+    return (        
+            <nav className="flex flex-row justify-between px-5  ">
+                    <p>logo</p>
+                    <p>menu</p>
+                </nav>        
+    )
+}
+
+export default NavBar

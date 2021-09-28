bd43748b94e7adf6e3e162ae67825c891bb13836
Hernan Curbelo <hernan.cur@hotmail.com>
2021-09-28 19:59:27 -0300

    Index conection Store

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -1,35 +1,38 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize } = require('sequelize');
 
 //Factory models
-const UsersFactory = require ('./User');
-// store
+const UsersFactory = require('./User');
+const StoreFactory = require('./Store.js');
 // address
 // products
 // orders
 // review
 // product type
 
-
 const sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-    {
-        logging: false, // set to console.log to see the raw SQL queries
-        native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-    }
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  {
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  }
 );
 
 //Conection with Sequelize
 const User = UsersFactory(sequelize);
-// store
+const Store = UsersFactory(StoreFactory);
 // address
 // products
 // orders
 // review
 // product type
- 
-
 
 module.exports = {
-    db: sequelize,
-    User
-}
+  db: sequelize,
+  User,
+  Store,
+  // address
+  // products
+  // orders
+  // review
+  // product type
+};

2adb651a28607e2415b50e08fd00c7f4cbcf3835
MatiMonas <matutin97@gmail.com>
2021-09-28 11:56:19 -0300

    Changes(tailwind.config.js) , adds color pallet

--- coco-mall/tailwind.config.js
+++ coco-mall/tailwind.config.js
@@ -1,11 +1,42 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                primary: '#ff4800',
+                blue: {
+                    450: '#5F99F7',
+                },
+            },
+        },
+        backgroundColor: (theme) => ({
+            ...theme('colors'),
+            primary: '#38A3A5',
+            secondary: '#2EC5CE',
+        }),
+        container: {
+            center: true,
+        },
+        screens: {
+            sm: '440px',
+            // => @media (min-width: 640px) { ... }
+
+            md: '547px',
+            // => @media (min-width: 768px) { ... }
+
+            lg: '768px',
+            // => @media (min-width: 1024px) { ... }
+
+            xl: '1024px',
+            // => @media (min-width: 1280px) { ... }
+
+            '2xl': '1680px',
+            // => @media (min-width: 1536px) { ... }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};

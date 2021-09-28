af12afff757ecd759859407934870311add5c1d5
MatiMonas <matutin97@gmail.com>
2021-09-28 12:50:17 -0300

    Landing page init

--- coco-mall/src/Views/Landing.jsx
+++ coco-mall/src/Views/Landing.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import MainButton from '../Components/MainButton';
+import SecondaryButton from '../Components/SecondaryButton';
 
-const Landing = () => {
+function Landing() {
     return (
-        <div>
-           Hello 
+        <div className="flex container p-10">
+            <MainButton text='Hello' />
+            <SecondaryButton text="Bye"/>
         </div>
-    )
+    );
 }
 
-export default Landing
+export default Landing;

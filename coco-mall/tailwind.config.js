38e4633b8f242b187b38e72c9766284e3da9ac2e
MatiMonas <matutin97@gmail.com>
2021-09-28 16:13:54 -0300

    adds border-color pallete

--- coco-mall/tailwind.config.js
+++ coco-mall/tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    darkMode: false, // or 'media' or 'class'
+    darkMode: "class", // or 'media' or 'class'
     theme: {
         extend: {
             colors: {
@@ -24,24 +24,26 @@ module.exports = {
             secondary: '#2EC5CE',
             'secondary-light': 'RGB(175, 218, 189)',
         }),
+        borderColor: (theme) => ({
+            ...theme('colors'),
+            primary: '#38A3A5',
+            'primary-light': 'RGB(175, 218, 219)',
+            secondary: '#2EC5CE',
+            'secondary-light': 'RGB(175, 218, 189)',
+        }),
         container: {
             center: true,
         },
         screens: {
             sm: '440px',
-            // => @media (min-width: 640px) { ... }
 
             md: '547px',
-            // => @media (min-width: 768px) { ... }
 
             lg: '768px',
-            // => @media (min-width: 1024px) { ... }
 
             xl: '1024px',
-            // => @media (min-width: 1280px) { ... }
 
             '2xl': '1680px',
-            // => @media (min-width: 1536px) { ... }
         },
     },
     variants: {

92917393e50d88737347d140c2e105d96267da44
MatiMonas <matutin97@gmail.com>
2021-09-28 18:49:51 -0300

    add npm i aos

--- coco-mall/src/Views/Landing.jsx
+++ coco-mall/src/Views/Landing.jsx
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,  } from 'react';
+import Aos from "aos";
+import "aos/dist/aos.css";
 import MainButton from '../Components/Buttons/MainButton';
 import NavBar from '../Components/NavBar';
 import SecondaryButton from '../Components/Buttons/SecondaryButton';
@@ -6,16 +8,23 @@ import TitleAndDescription from '../Components/TitleAndDescription';
 import SectionHow from '../Components/Sections/SectionHow';
 import SectionBenefits from '../Components/Sections/SectionBenefits';
 
+
+
 function Landing() {
     const [width, setWidth] = useState(window.innerWidth);
     const breakpoint = 1024;
+
+ 
+    useEffect(() => {
+        Aos.init({duration:2000, offset:200})
+    },[])
 
     useEffect(() => {
         window.addEventListener('resize', () => setWidth(window.innerWidth));
     }, []);
 
     return (
-        <div className='grid grid-col-1 grid-rows-6 min-h-screen px-5 py-3'>
+        <div className='grid grid-col-1  min-h-screen px-5 py-3'>
             {width < breakpoint ? (
                 <div className='absolute right-0 -top-5 md:-top-10 lg:-top-28'>
                     <div className='w-52 h-52 sm:w-64 sm:h-64 md:w-80 md:h-80 lg:w-96 lg:h-96  bg-primary-light rounded-tl-full border border-primary-light  '></div>
@@ -27,24 +36,24 @@ function Landing() {
                 </div>
             )}
 
-            <div className=' col-span-1 row-span-2  z-10 '>
+            <div className=' col-span-1 mb-36  z-10 '>
                 <NavBar />
             </div>
-
-            <div className='col-span-1 row-span-1 z-10'>
+            
+            <div className='col-span-1 mb-20 z-10'>
                 <TitleAndDescription />
             </div>
 
-            <div className='flex col-span-1 row-start-5 row-end-6'>
+            <div  className='flex col-span-1 mb-64 '>
                 <MainButton text='Client' />
                 <SecondaryButton text='Shop' />
             </div>
 
-            <div className='col-span-1'>
+            <div data-aos="fade-right" className='col-span-1  mb-56'>
                 <SectionHow />
             </div>
 
-            <div className='col-span-1'>
+            <div data-aos="fade-up" className='col-span-1 mb-56'>
                 <SectionBenefits />
             </div>
         </div>

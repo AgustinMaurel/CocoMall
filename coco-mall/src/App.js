87b1120f755c31d9641659aee83020a68b581a9e
AgustinMaurel <85203984+AgustinMaurel@users.noreply.github.com>
2021-09-27 22:38:28 -0300

    init files

---
+++ coco-mall/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="bg-red-500">BIENVENUTE A COCOmall</h1>
+    </div>
+  );
+}
+
+export default App;

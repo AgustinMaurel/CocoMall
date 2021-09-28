918044dd7d2854dcbbd2fbf59de955bbe68add2e
Prym003 <85133493+Prym003@users.noreply.github.com>
2021-09-28 20:09:35 -0300

    prueba

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -1,11 +1,6 @@
 const { Sequelize } = require('sequelize');
 
 //Factory models
-<<<<<<< HEAD
-const UsersFactory = require ('./User');
-const OrderFactory = require ('./Order')
-
-=======
 const UsersFactory = require('./User');
 const StoreFactory = require('./Store.js');
 // address
@@ -13,7 +8,6 @@ const StoreFactory = require('./Store.js');
 // orders
 // review
 // product type
->>>>>>> bd43748b94e7adf6e3e162ae67825c891bb13836
 
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
@@ -25,16 +19,6 @@ const sequelize = new Sequelize(
 
 //Conection with Sequelize
 const User = UsersFactory(sequelize);
-<<<<<<< HEAD
-const Order = OrderFactory(sequelize)
-
-
-module.exports = {
-    db: sequelize,
-    User,
-    Order,
-}
-=======
 const Store = UsersFactory(StoreFactory);
 // address
 // products
@@ -52,4 +36,3 @@ module.exports = {
   // review
   // product type
 };
->>>>>>> bd43748b94e7adf6e3e162ae67825c891bb13836

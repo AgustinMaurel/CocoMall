678dc00da4635f3ef5648d7a839e2c4f507efd74
matimonas <matutin97@gmail.com>
2021-09-27 23:21:15 -0300

    prettier config done

--- coco-mall/src/App.js
+++ coco-mall/src/App.js
@@ -1,10 +1,9 @@
-import "./App.css";
+import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <h1 className="bg-red-500">BIENVENUTE A COCOmall</h1>
-            
+        <div className='App'>
+            <h1 className='bg-red-500'>BIENVENUTE A COCOmall</h1>
         </div>
     );
 }

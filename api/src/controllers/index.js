4f599af620d4d6e368f7f5587399f1ed5367ba15
Prym003 <85133493+Prym003@users.noreply.github.com>
2021-09-28 20:44:59 -0300

    db-update Universal Controllers

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -1,8 +1,64 @@
 class ModelController {
-    constructor (model) {
+    constructor(model) {
         this.model = model;
     }
-    //general Functions
+    //general Functions    
+    createData = (req, res) => {
+        let data = req.body;
+        this.model.create(data).
+            then((data) => {
+                res.send(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    findDataId = (req, res) => {
+        this.model.findById(req.params.id).
+            then((data) => {
+                res.send(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    deleteDataById = (req, res) => {
+        this.model.deleteById(req.params.id).
+            then((data) => {
+                res.status(200).json({
+                    message: `${data}deleted successfully`,
+                    data,
+                })
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    updateData = (req, res) => {
+        this.model.updateData(req.body, req.params.id).
+            then((data) => {
+                res.status(200).json({
+                    message: "Gig updated successfully",
+                    data,
+                })
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    findData = (req, res) => {
+        this.model.findAll().
+            then((data) => {
+                res.send(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 }
 
 module.exports = ModelController

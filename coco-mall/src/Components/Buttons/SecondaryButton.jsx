dbf655599fc0f24c2448f45dae1a81794782aa41
MatiMonas <matutin97@gmail.com>
2021-09-29 16:16:24 -0300

    add responsiveness to font size in how it works section

--- coco-mall/src/Components/Buttons/SecondaryButton.jsx
+++ coco-mall/src/Components/Buttons/SecondaryButton.jsx
@@ -4,8 +4,7 @@ import { Link } from 'react-router-dom';
 function SecondaryButton({ text }) {
     return (
         <div
-            className='shadow-lg flex items-center justify-center bg-white  border border-primary  text-primary w-40 rounded-md h-8 
-            md:w-32
+            className='shadow-lg flex items-center justify-center bg-white  border border-primary  text-primary w-40 rounded-md h-8             
             xl:border-none xl:shadow-none xl:bg-secondary-light xl:h-12 xl:w-44  '
         >
             <Link className='relative w-full h-full flex items-center align-center justify-center ' to='/'>

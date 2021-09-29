18841f1e51b4f128e098b01d4318e175ba7c86f9
MatiMonas <matutin97@gmail.com>
2021-09-29 16:16:32 -0300

    adds responsiveness

--- coco-mall/src/Components/Sections/SectionBenefits.jsx
+++ coco-mall/src/Components/Sections/SectionBenefits.jsx
@@ -13,6 +13,8 @@ const SectionBenefits = () => {
                 <div className='h-10 w-10 bg-primary-light rounded-full absolute z-0 left-1/3'></div>
                 <div className='h-24 w-24 bg-primary-light rounded-full absolute z-0 left-2/3 top-60 '></div>
             </div>
+
+            
             <div className='flex flex-col items-center justify-center h-screen pb-10 '>
                 <h3 className='text-xl font-bold pb-10 after z-10'>Benefits of working with us</h3>
                 <div className='flex flex-col items-center gap-10'>

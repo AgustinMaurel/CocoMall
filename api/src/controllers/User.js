2ae1a8dbc7c6cf04d11f7e89c5fd686e6ecd1a85
Daniel Zapata <daniel.zapata.grajales@gmail.com>
2021-09-29 17:14:28 -0500

    User v1

--- api/src/controllers/User.js
+++ api/src/controllers/User.js
@@ -1,13 +1,29 @@
-const { User } = require('../models/index')
-const ModelController = require('./index')
-
+const {User} = require("../models/index");
+const ModelController = require("./index");
+const {v4: uuidv4} = require("uuid");
 class UserModel extends ModelController {
     constructor(model) {
         super(model);
     }
+    createUser = (req, res) => {
+        const {Name, LastName, Mail} = req.body;
+        
+        try {
+            User.create({
+                id: uuidv4(),
+                Name: Name,
+                LastName: LastName,
+                Mail: Mail,
+            }).then((e) => {
+                return res.json(e);
+            });
+        } catch (err) {
+            return res.status(400).json(err);
+        }
+    };
     //Specific Functions for this model
 }
 
-const UserController = new UserModel(User)
+const UserController = new UserModel(User);
 
-module.exports = UserController
+module.exports = UserController;

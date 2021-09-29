9d8040c403c8373ef870087be0e952b72a087823
MatiMonas <matutin97@gmail.com>
2021-09-28 22:59:37 -0300

    adds bk color to secondary button

--- coco-mall/src/Components/Buttons/SecondaryButton.jsx
+++ coco-mall/src/Components/Buttons/SecondaryButton.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function SecondaryButton({ text }) {
     return (
-        <div className='shadow-lg flex items-center justify-center  border border-primary- text-primary w-40 md:w-32 h-8 p-2 rounded-md'>
+        <div className='shadow-lg flex items-center justify-center bg-white  border border-primary- text-primary w-40 md:w-32 h-8 p-2 rounded-md'>
         <Link to="/">
                 <button className=' focus:outline-none text-center text-base md:text-lg'>{text}</button>
         </Link>

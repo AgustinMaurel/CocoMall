f6f6b9032b43f70febbd69563a11c2a661681b17
Prym003 <85133493+Prym003@users.noreply.github.com>
2021-09-29 10:58:30 -0300

    conection fixed DB

--- api/src/models/Review.js
+++ api/src/models/Review.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  sequelize.define('Store', {
+  return sequelize.define('Store', {
     id: {
       type: DataTypes.UUID,
       primaryKey: true,
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,6 +4,8 @@ const router = Router();
 
 //se importa la ruta y se la agrega
 //modelo
-// router.use('/', 'Ruta')
+router.use('/', (req,res)=>{
+    res.send('hola mundo')
+})
 
 module.exports = router;

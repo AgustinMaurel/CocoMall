dc6a94a7d7051c36c64a93505aefde920a13a5d1
chris <silvero355@gmail.com>
2021-09-28 23:11:39 -0300

    add changes in SectionStats

--- coco-mall/src/Components/Sections/SectionStats.jsx
+++ coco-mall/src/Components/Sections/SectionStats.jsx
@@ -8,27 +8,29 @@ import icon07 from '../../Assets/icons/07.png';
 const SectionStats = () => {
     return (
         
-        <div className='flex justify-center  h-screen items-center '>
-            <div className='bg-primary flex gap-4 items-center h-32  p-4'>
-                <div className='flex flex-col items-center rounded-md bg-white'>
-                    <img src={icon01} alt='create' />
-                    <p>40+</p>
-                    <p>Happy Clients</p>
-                </div>
-                <div className='flex flex-col items-center rounded-md bg-white'>
-                    <img src={icon04} alt='create' />
-                    <p>540+</p>
-                    <p>Cantidad de tiendas</p>
-                </div>
-                <div className='flex flex-col items-center rounded-md bg-white'>
-                    <img src={icon06} alt='create' />
-                    <p>300</p>
-                    <p>Usuarios registrados</p>
-                </div>
-                <div className='flex flex-col items-center rounded-md bg-white'>
-                    <img src={icon07} alt='create' />
-                    <p>25+</p>
-                    <p>Awards Won</p>
+        <div className='flex justify-center h-screen items-center'>
+            <div className='bg-primary flex justify-center w-screen h-32 relative'>
+                <div className='flex gap-4 items-center justify-center h-32 absolute top-16'>
+                    <div className='flex flex-col items-center justify-around rounded-md bg-white w-32 h-32'>
+                        <img src={icon01} alt='create' />
+                        <p className='font-extrabold text-center text-5xl text-primary '>40+</p>
+                        <p className='text-xs'>Happy Clients</p>
+                    </div>
+                    <div className='flex flex-col items-center justify-around rounded-md bg-white w-32 h-32'>
+                        <img src={icon04} alt='create' />
+                        <p className='font-extrabold text-center text-5xl text-primary '>540+</p>
+                        <p className='text-xs'>Cantidad de tiendas</p>
+                    </div>
+                    <div className='flex flex-col items-center justify-around rounded-md bg-white w-32 h-32'>
+                        <img src={icon06} alt='create' />
+                        <p className='font-extrabold text-center text-5xl text-primary '>300</p>
+                        <p className='text-xs'>Usuarios registrados</p>
+                    </div>
+                    <div className='flex flex-col items-center justify-around rounded-md bg-white w-32 h-32'>
+                        <img src={icon07} alt='create' />
+                        <p className='font-extrabold text-center text-5xl text-primary '>25+</p>
+                        <p className='text-xs'>Awards Won</p>
+                    </div>
                 </div>
             </div>
         </div>

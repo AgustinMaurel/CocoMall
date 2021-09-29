eeda9e30554267e1d7581ad13000bdb619a64fb2
chris <silvero355@gmail.com>
2021-09-28 21:18:11 -0300

    new branch - Landing

---
+++ coco-mall/src/Components/Sections/SectionStats.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SectionStats = () => {
+    return (
+        <div>
+            <p>Section Stats</p>
+        </div>
+    );
+};
+
+export default SectionStats;

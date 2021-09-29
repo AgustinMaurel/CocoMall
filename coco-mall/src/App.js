559fd81bce17e04e8df219395d2b8bf2a8080817
PabloK09 <pablokollm@gmail.com>
2021-09-28 22:16:13 -0300

    Creacion de ruta para el login y register

--- coco-mall/src/App.js
+++ coco-mall/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Landing from './Views/Landing';
+import LoginScreen from './Views/Auth/LoginScreen';
+import RegisterScreen from './Views/Auth/RegisterScreen';
 
 function App() {
     return (
         <>
             <Switch>
                 <Route path='/' exact component={Landing} />
+                <Route path='/auth/login' exact component={LoginScreen} />
+                <Route path='/auth/register' exact component={RegisterScreen} />
             </Switch>
         </>
     );

0361833f03157ce7c66882841bcb0e30cd81bab4
MatiMonas <matutin97@gmail.com>
2021-09-29 15:59:52 -0300

    responsive changes

--- coco-mall/src/Components/Sections/SectionStats.jsx
+++ coco-mall/src/Components/Sections/SectionStats.jsx
@@ -6,14 +6,18 @@ import icon06 from '../../Assets/icons/06.png';
 import icon07 from '../../Assets/icons/07.png';
 
 const SectionStats = () => {
-    return (  
+    return (
         <div className='flex justify-center h-screen items-center'>
-            <div className='bg-primary flex justify-center w-full h-60 relative
-                            lg:h-32'>
-                <div className='flex gap-8 items-center justify-center h-32 absolute top-10
+            <div
+                className='bg-primary flex justify-center w-full h-60 relative
+                            lg:h-32'
+            >
+                <div
+                    className='flex gap-8 items-center justify-center h-32 absolute top-10
                                 sm:flex-wrap
                                 md:flex-wrap md:mx-12
-                                lg:absolute lg:top-16' >
+                                lg:absolute lg:top-16'
+                >
                     <div className='flex flex-col items-center justify-around rounded-md bg-white w-32 h-32 shadow'>
                         <img src={icon01} alt='create' />
                         <p className='font-extrabold text-center text-5xl text-primary '>40+</p>

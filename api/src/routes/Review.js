5610e0d09981cdcc6ed4cebb85689671af7eedcd
Prym003 <85133493+Prym003@users.noreply.github.com>
2021-09-30 11:37:46 -0300

    db-update route CREATE REVIEW

--- api/src/routes/Review.js
+++ api/src/routes/Review.js
@@ -1,9 +1,11 @@
 const { Router } = require("express")
 const router = Router();
-const { getAllData } = require('../controllers/Review.js')
+const { getAllData, createReview } = require('../controllers/Review.js')
 
 
 //all routes start with /review
 router.get('/', getAllData)
+
+router.get('/create', createReview)
 
 module.exports = router

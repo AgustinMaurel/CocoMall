02d0199726fdb7fa8298ccdcc6d8c549867639a0
chris <silvero355@gmail.com>
2021-09-30 22:19:28 -0300

    Add image upload and preview

--- coco-mall/src/Views/ProductsCreate.jsx
+++ coco-mall/src/Views/ProductsCreate.jsx
@@ -1,10 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import Input from '../Components/Inputs/Input';
 
-const IMG_DEFAULT = 'https://www.sinrumbofijo.com/wp-content/uploads/2016/05/default-placeholder.png'
+const IMG_DEFAULT =
+    'https://www.sinrumbofijo.com/wp-content/uploads/2016/05/default-placeholder.png';
+
+const validate = {
+    product: {
+        required: { value: true, message: 'product is required' },
+        minLength: {
+            value: 2,
+            message: 'product must contain at least 2 characters',
+        },
+        maxLength: {
+            value: 16,
+            message: 'product must contain a maximum of 16 characters ',
+        },
+    },
+    description: {
+        required: { value: true, message: 'description is required' },
+        minLength: {
+            value: 2,
+            message: 'description must contain at least 2 characters',
+        },
+        maxLength: {
+            value: 40,
+            message: 'description must contain a maximum of 40 characters ',
+        },
+    },
+    price: {
+        required: { value: true, message: 'description is required' },
+        min: {
+            value: 1,
+            message: 'price min',
+        },
+        max: {
+            value: 9999999999,
+            message: 'price maximum',
+        },
+    },
+    image: { required: true },
+};
 
 const ProductsCreate = () => {
+    const [image, setImage] = useState("");
+    const [isUploaded, setIsUploaded] = useState(false);
+    //const [typeFile, setTypeFile] = useState("");
+
+    const handleImageChange = (e) => {
+        if (e.target.files && e.target.files[0]) {
+          //setTypeFile(e.target.files[0].type);
+          let reader = new FileReader();
+    
+          reader.onload = (e) => {
+            setImage(e.target.result);
+            setIsUploaded(true);
+          };
+    
+          reader.readAsDataURL(e.target.files[0]);
+        }
+      }
+
     const {
         register,
         handleSubmit,
@@ -12,9 +68,9 @@ const ProductsCreate = () => {
         formState: { errors },
     } = useForm();
 
-    const onSubmit = (data) => (
-        console.log(data)
-        );
+    const onSubmit = (data) => console.log(data);
+
+    console.log(watch('image'));
 
     return (
         <div className='flex flex-col justify-center items-center w-screen h-screen '>
@@ -29,12 +85,14 @@ const ProductsCreate = () => {
                         errors={errors}
                         name='name'
                         msg='This field is required'
+                        validate={validate.product}
                     />
                     <Input
                         register={register}
                         errors={errors}
                         name='description'
                         msg='This field is required'
+                        validate={validate.description}
                     />
                     <Input
                         register={register}
@@ -42,12 +100,16 @@ const ProductsCreate = () => {
                         name='price'
                         msg='This field is required'
                         type='number'
+                        validate={validate.price}
                     />
                     <Input
                         register={register}
                         errors={errors}
                         name='image'
                         msg='This field is required'
+                        type='file'
+                        validate={validate.image}
+                        onChange={handleImageChange}
                     />
                     <div className='flex items-center justify-between'>
                         <input
@@ -57,10 +119,14 @@ const ProductsCreate = () => {
                     </div>
                 </form>
                 <div className='border bg-white shadow-md rounded p-8 m-4 w-1/3 text-center'>
-                    <img src={watch('image') ? watch('image') : IMG_DEFAULT} alt="img" />
-                    <p className='font-bold mt-5 text-2xl'>{watch('name') ? watch('name').toUpperCase() : 'NAME'}</p>
-                    <p className='mt-1'>{watch('description') ? watch('description') : 'description'}</p>
-                    <p className='text-xl' >{watch('price') ? `$ ${watch('price')}` : '$0.00'}</p>
+                    <img src={isUploaded ? image : IMG_DEFAULT} alt='img' />
+                    <p className='font-bold mt-5 text-2xl'>
+                        {watch('name') ? watch('name').toUpperCase() : 'NAME'}
+                    </p>
+                    <p className='mt-1'>
+                        {watch('description') ? watch('description') : 'description'}
+                    </p>
+                    <p className='text-xl'>{watch('price') ? `$ ${watch('price')}` : '$0.00'}</p>
                 </div>
             </div>
         </div>

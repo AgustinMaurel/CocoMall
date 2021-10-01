d082a3dc311ba50809942030a1e922b9ec2c1ff5
MatiMonas <matutin97@gmail.com>
2021-10-01 15:01:04 -0300

    Adds link to home

--- coco-mall/src/Components/NavBar.jsx
+++ coco-mall/src/Components/NavBar.jsx
@@ -13,9 +13,14 @@ function NavBar() {
         <>
             {width < breakpoint ? (
                 <nav className='flex justify-between'>
-                    <p>Logo</p>
                     <div>
-                        <Link  to='/'>
+                        <p>Logo</p>
+                        <Link to='/'>
+                            <p>Home</p>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to='/'>
                             <svg
                                 className='w-6 h-6'
                                 fill='none'
@@ -35,19 +40,28 @@ function NavBar() {
                 </nav>
             ) : (
                 <nav className='flex align-center items-center h-auto justify-between  w-full'>
-                    <div>
+                    <div className='flex align-center items-center gap-5'>
                         <p>Logo</p>
+                        <Link to='/'>
+                            <p>Home</p>
+                        </Link>
                     </div>
                     <div className='flex gap-x-5'>
                         <div className='shadow  flex items-center justify-center align-center bg-primary h-8  w-24  rounded'>
-                            <Link className='relative w-full h-full flex items-center align-center justify-center' to='/auth/login'>
+                            <Link
+                                className='relative w-full h-full flex items-center align-center justify-center'
+                                to='/auth/login'
+                            >
                                 <button className='w-full focus:outline-none text-white text-center text-sm text-md'>
                                     Login
                                 </button>
                             </Link>
                         </div>
                         <div className='shadow  flex items-center justify-center  align-center bg-secondary-light  h-8 w-24   rounded'>
-                            <Link className='relative w-full h-full flex items-center align-center justify-center' to='/auth/register'>
+                            <Link
+                                className='relative w-full h-full flex items-center align-center justify-center'
+                                to='/auth/register'
+                            >
                                 <button className='w-full h-full focus:outline-none text-primary text-center text-sm text-md'>
                                     Sign Up
                                 </button>

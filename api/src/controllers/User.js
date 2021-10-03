f6447725d4d57110dc79989eb0f905dceb2d908c
Prym003 <85133493+Prym003@users.noreply.github.com>
2021-10-03 20:23:37 -0300

    db-update GET User By Id

--- api/src/controllers/User.js
+++ api/src/controllers/User.js
@@ -1,4 +1,4 @@
-const {User, Store, Address} = require("../models/index");
+const { User, Store, Address } = require("../models/index");
 const ModelController = require("./index");
 class UserModel extends ModelController {
     constructor(model) {
@@ -6,38 +6,66 @@ class UserModel extends ModelController {
     }
     //Specific Functions for this model
 
-    createUser = (req, res) => {
-        const {Name, LastName, Mail} = req.body;
-        try {
-            User.create({
-                Name,
-                LastName,
-                Mail,
-            }).then((e) => {
-                return res.json(e);
-            });
-        } catch (err) {
-            return res.status(400).json(err);
+    createUser = async (req, res) => {
+        const user = req.body;
+        if(typeof user === "object"){
+            try {
+                let newUser = await this.model.create(user)
+                res.send(newUser)
+            } catch (error) {
+                res.json(error);
+            }
+        } else {
+            res.status(400).send("Wrong parameters")
         }
     };
 
-    getAllData = async (req, res, next) => {
-        let data = await User.findAll({
-            include: [
-                //include the related tables and the specific cloumn that they have attached
-                {
-                    model: Store,
-                    attributes: ["storeName"],
+    getAllData = async (req, res) => {
+        try {
+            let user = await this.model.findAll({
+                include: [
+                    //include the related tables and the specific cloumn that they have attached
+                    {
+                        model: Store,
+                        attributes: ["storeName"],
+                    },
+                    {
+                        model: Address,
+                        attributes: ["directions"],
+                    },
+                ],
+            })
+            res.send(user);
+        } catch (error) {
+            res.send(error)
+        }
+
+    };
+
+    getUserById = async (req, res) => {
+        const id = req.params.id
+        try {
+            let user = await this.model.findAll({
+                where:{
+                    id: id
                 },
-                {
-                    model: Address,
-                    attributes: ["directions"],
-                },
-            ],
-        }).catch((err) => {
-            next(err);
-        });
-        res.send(data);
+                include: [
+                    //include the related tables and the specific cloumn that they have attached
+                    {
+                        model: Store,
+                        attributes: ["storeName"],
+                    },
+                    {
+                        model: Address,
+                        attributes: ["directions"],
+                    },
+                ],
+            })
+            res.send(user);
+        } catch (error) {
+            res.json(error);
+        }
+
     };
 
     bulkCreateUser = async (req, res, next) => {
--- api/src/routes/User.js
+++ api/src/routes/User.js
@@ -1,9 +1,17 @@
 const { Router } = require('express');
 const router = Router();
-const { getAllData, createUser, bulkCreateUser } = require('../controllers/User.js');
+const { getAllData, createUser, bulkCreateUser, getUserById} = require('../controllers/User.js');
 
 //all routes start with /user
+
+//GET's
 router.get('/', getAllData);
+
+router.get('/:id', getUserById);
+
+//POST's
 router.post('/create', createUser);
+
 router.post('/bulkCreate', bulkCreateUser)
+
 module.exports = router;

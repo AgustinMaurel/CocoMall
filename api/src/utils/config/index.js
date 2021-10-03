0d49629f92e48249b5041fb0f04115315210db94
Hernan Curbelo <hernan.cur@hotmail.com>
2021-10-03 16:43:11 -0300

    Cloudinary working

--- api/src/utils/config/index.js
+++ api/src/utils/config/index.js
@@ -4,6 +4,7 @@ module.exports = {
     DB_USER: process.env.DB_USER || "postgres",
     DB_PASSWORD: process.env.DB_PASSWORD || 'romilinda0100',
     DB_HOST: process.env.DB_HOST || "localhost",
-    DB_NAME: process.env.DB_NAME || 'cocoMall',
+    DB_NAME: process.env.DB_NAME || 'cocomall',
     PORT: process.env.PORT || 3001,
 }
+

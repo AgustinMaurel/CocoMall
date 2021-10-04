9035bc9bc8770752230a6f2ccbc433cb0ffaab19
Prym003 <85133493+Prym003@users.noreply.github.com>
2021-10-03 22:05:48 -0300

    dasd

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -2,32 +2,6 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
     return sequelize.define(
-<<<<<<< HEAD
-        'User', {
-        id: {
-            type: DataTypes.STRING,
-           /*  defaultValue: DataTypes.UUIDV4, */
-            allowNull: false,
-            primaryKey: true,
-            unique: true
-        },
-        Name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: false
-        },
-        LastName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: false
-        },
-        Mail: {
-            type: DataTypes.STRING,
-            validate: {
-                isEmail: true,
-                notEmpty: true,
-                notNull: false
-=======
         'User',
         {
             id: {
@@ -53,7 +27,6 @@ module.exports = (sequelize) => {
                 },
                 allowNull: false,
                 unique: true,
->>>>>>> bb7fc35f2153389cb5d1179585885c9f768be307
             },
         },
         {

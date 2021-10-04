add1090f3c596d97c8b1529f001d4d40092aa33c
AgustinMaurel <85203984+AgustinMaurel@users.noreply.github.com>
2021-10-03 21:31:59 -0300

    commit

--- api/index.js
+++ api/index.js
@@ -4,12 +4,6 @@ require('dotenv').config();
 const { PORT } = require('./src/utils/config/index');
 
 // Syncing all the models at once.
-<<<<<<< HEAD
-db.sync({ force: true })
-  .then(() => {
-    server.listen(PORT, () => {
-      console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
-=======
 db.sync()
     .then(() => {
         server.listen(PORT, () => {
@@ -18,5 +12,4 @@ db.sync()
     })
     .catch((err) => {
         console.error(err);
->>>>>>> bb7fc35f2153389cb5d1179585885c9f768be307
     });

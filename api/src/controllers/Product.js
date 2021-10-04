1f201864162926480ef7807fbcd2032a5b90b157
Prym003 <85133493+Prym003@users.noreply.github.com>
2021-10-03 23:29:30 -0300

    asdas

--- api/src/controllers/Product.js
+++ api/src/controllers/Product.js
@@ -23,7 +23,8 @@ class ProductModel extends ModelController {
                 );
                 let public_id = uploadedResponse.public_id;
                 //Get the Product from body
-                const product = { ...req.body.product, cloudImage: public_id };
+                const product = { ...req.body.product, cloudImage: public_id ? public_id : 'No image id' };
+                console.log(product)
                 //Create new Product
                 const newProduct = await this.model.create(product);
                 const productId = newProduct.id;

771e41f9589e671a41c1d6e632d0bc118927ebd3
MatiMonas <matutin97@gmail.com>
2021-10-04 21:41:12 -0300

    adds cart to navbar, makes nav sticky

--- coco-mall/src/Components/NavBar.jsx
+++ coco-mall/src/Components/NavBar.jsx
@@ -8,19 +8,18 @@ function NavBar() {
     const [width, setWidth] = useState(window.innerWidth);
     const breakpoint = 1024;
 
-    const history = useHistory()
-    const user = useSelector((state) => state.auth)
-    const dispatch = useDispatch()
+    const history = useHistory();
+    const user = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
 
     useEffect(() => {
         window.addEventListener('resize', () => setWidth(window.innerWidth));
     }, []);
 
-
-    function handleLogout (e) {
-        e.preventDefault()
-        dispatch(startLogout())
-        history.push('/')
+    function handleLogout(e) {
+        e.preventDefault();
+        dispatch(startLogout());
+        history.push('/');
     }
 
     return (
@@ -55,43 +54,62 @@ function NavBar() {
                     </div>
                 </nav>
             ) : (
-                <nav className='flex align-center items-center h-auto justify-between  w-full'>
-                    <div className='flex align-center items-center  gap-5'>
-                        <Link to='/'>
-                            <p>Logo</p>
-                        </Link>
-                        <Link to='/home'>
-                            <p>Home</p>
-                        </Link>
-                    </div>
-                    <div className='flex gap-x-5'>
-                        {user.uid ? (
-                            <>
-                                <div className='shadow  flex items-center justify-center align-center bg-primary h-8  w-24  rounded'>
-                                    <Link
-                                        className='relative w-full h-full flex items-center align-center justify-center'
-                                        to='/auth/login'
-                                    >
-                                        <button className='w-full focus:outline-none text-white text-center text-sm text-md'>
-                                            {user.name || "Pablo Koll"}
-                                        </button>
-                                    </Link>
-                                </div>
-                                <div className='shadow  flex items-center justify-center  align-center bg-secondary-light  h-8 w-24   rounded'>
-                                    <div
-                                        className='relative w-full h-full flex items-center align-center justify-center'
-                                        to='/'
-                                    >
-                                        <button onClick={handleLogout} 
-                                        className='min-w-max h-full focus:outline-none text-primary text-center text-sm text-md'>
-                                            Logout
-                                        </button>
+                <div className='flex top-0 shadow-lg sticky  w-full bg-gray-100 h-14 pt-4 border-b-2 border-gray-100 px-20 pb-3 z-10 '>
+                    <nav className=' flex align-center items-center h-auto justify-between  w-full'>
+                        <div className='flex align-center items-center  gap-5'>
+                            <Link to='/'>
+                                <p>Logo</p>
+                            </Link>
+                            <Link to='/home'>
+                                <p>Home</p>
+                            </Link>
+                        </div>
+                        <div className='flex gap-x-5 items-center'>
+                            {user.uid ? (
+                                <>
+                                    <div>
+                                        <Link to='/home'>
+                                            <svg
+                                                className='w-6 h-6'
+                                                fill='none'
+                                                stroke='currentColor'
+                                                viewBox='0 0 24 24'
+                                                xmlns='http://www.w3.org/2000/svg'
+                                            >
+                                                <path
+                                                    strokeLinecap='round'
+                                                    strokeLinejoin='round'
+                                                    stroke-width='2'
+                                                    d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
+                                                ></path>
+                                            </svg>
+                                        </Link>
                                     </div>
-                                </div>
-                            </>
-                            )
-
-                            : (
+                                    <div className='shadow  flex items-center justify-center align-center bg-primary h-8  w-24  rounded'>
+                                        <Link
+                                            className='relative w-full h-full flex items-center align-center justify-center'
+                                            to='/auth/login'
+                                        >
+                                            <button className='w-full focus:outline-none text-white text-center text-sm text-md'>
+                                                {user.name || 'Pablo Koll'}
+                                            </button>
+                                        </Link>
+                                    </div>
+                                    <div className='shadow  flex items-center justify-center  align-center bg-secondary-light  h-8 w-24   rounded'>
+                                        <div
+                                            className='relative w-full h-full flex items-center align-center justify-center'
+                                            to='/'
+                                        >
+                                            <button
+                                                onClick={handleLogout}
+                                                className='min-w-max h-full focus:outline-none text-primary text-center text-sm text-md'
+                                            >
+                                                Logout
+                                            </button>
+                                        </div>
+                                    </div>
+                                </>
+                            ) : (
                                 <>
                                     <div className='shadow  flex items-center justify-center align-center bg-primary h-8  w-24  rounded'>
                                         <Link
@@ -114,10 +132,10 @@ function NavBar() {
                                         </Link>
                                     </div>
                                 </>
-                            )
-                        }
-                    </div>
-                </nav>
+                            )}
+                        </div>
+                    </nav>
+                </div>
             )}
         </>
     );

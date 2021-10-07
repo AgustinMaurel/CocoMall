277e4fac3b1f935fd9f9de0be7142892f310199b
MatiMonas <matutin97@gmail.com>
2021-10-07 13:52:27 -0300

    adds mercadopago checkout

--- api/src/routes/Checkout.js
+++ api/src/routes/Checkout.js
@@ -1,12 +1,12 @@
-const {Router} = require("express");
+const { Router } = require('express');
 const router = Router();
-const {stripe} = require("../models/index");
-const mercadopago = require("mercadopago");
+const { stripe } = require('../models/index');
+const mercadopago = require('mercadopago');
 
 //all this rputes strart with checkout
-router.post("/stripe", async (req, res) => {
+router.post('/stripe', async (req, res) => {
     try {
-        const {amount, id, currency, description} = req.body;
+        const { amount, id, currency, description } = req.body;
         const payment = await stripe.paymentIntents.create({
             amount,
             currency,
@@ -15,39 +15,39 @@ router.post("/stripe", async (req, res) => {
             confirm: true,
         });
         console.log(payment);
-        res.send("pagado papu");
+        res.send('pagado papu');
     } catch (error) {
         res.send(error.raw.message);
     }
 });
 
-router.post("/mercadopago", async (req, res) => {
+router.post('/mercadopago', async (req, res) => {
+    const { title, total } = req.body;
     mercadopago.configure({
         access_token:
-            "TEST-4584026682195569-100518-6865fd891a74a50438b28e4a07dae8f4-835549336",
+            'TEST-4584026682195569-100518-6865fd891a74a50438b28e4a07dae8f4-835549336',
     });
 
     let preference = {
         items: [
             {
-                title: req.body.title,
-                unit_price: req.body.total,
+                title: title,
+                unit_price: total,
                 quantity: 1,
             },
         ],
         back_urls: {
-            success: "http://localhost:3001/feedback",
-            failure: "http://localhost:3001/feedback",
-            pending: "http://localhost:3001/feedback",
+            success: 'http://localhost:3001/feedback',
+            failure: 'http://localhost:3001/feedback',
+            pending: 'http://localhost:3001/feedback',
         },
-        auto_return: "approved",
+        auto_return: 'approved',
     };
     let answer = await mercadopago.preferences.create(preference);
     const response = answer.body.id;
     const init_points = answer.body.init_point;
-    res.send(response, init_points);
+    console.log('RESPONSE: ', response, 'INIT POINT: ', init_points);
+    res.json({response: response, init_points: init_points});
 });
-
-
 
 module.exports = router;

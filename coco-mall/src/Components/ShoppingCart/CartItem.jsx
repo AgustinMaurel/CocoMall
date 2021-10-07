300c935cb1f577f34cc02f92ff361307f1c6b066
MatiMonas <matutin97@gmail.com>
2021-10-07 13:42:25 -0300

    changes directory of buying button

--- coco-mall/src/Components/ShoppingCart/CartItem.jsx
+++ coco-mall/src/Components/ShoppingCart/CartItem.jsx
@@ -1,25 +1,13 @@
 
 
 const CartItem = ({ data, deleteFromCart, deleteAllFromCart }) => {
-    let { id, name, price, quantity } = data;
+    let { id, productName, price, quantity } = data;
 
 
-    //post a
-    //http://localhost:3001/checkout/mercadopago
-    /*
-    let objetct = {
-        title: "coco remera",
-        unit_price: 100,
-        quantity: 1,
-    }
-    */
-   //back devuelve un json con la url a redireccionar
-   //
-  
     return (
         <div className='flex flex-col relative  justify-between bg-red-500'>
             <div className='border-b-2 '>
-                <h3>{name}</h3>
+                <h3>{productName}</h3>
                 <p className='font-semibold'>
                     ${price}.00  {quantity > 1 && `x${quantity} = $${price*quantity}`}
                 </p>
@@ -34,12 +22,12 @@ const CartItem = ({ data, deleteFromCart, deleteAllFromCart }) => {
                     </button>
                 </div>
             </div>
-            <button className="border">Comprar</button>
+           
         </div>
     );
 };
 
-//el back me manda una url y tengo que redireccionar a esa url
+
 
 
 

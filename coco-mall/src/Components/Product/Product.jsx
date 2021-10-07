eeddf48348de697d5a9142d5c1845af70ae20fef
PabloK09 <pablokollm@gmail.com>
2021-10-07 18:50:24 -0300

    Chris haciendo magia

--- coco-mall/src/Components/Product/Product.jsx
+++ coco-mall/src/Components/Product/Product.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { addToCart } from '../../Redux/actions/shoppingActions'
+import React from 'react';
 import { useDispatch } from 'react-redux'
 import { useParams, Link, useLocation } from 'react-router-dom';
+
+import { addToCart } from '../../Redux/actions/shoppingActions'
 import { getProductDetail } from '../../Redux/actions/stores'
 
 export default function Product(props) {
@@ -10,18 +11,26 @@ export default function Product(props) {
     const { product } = props
 
     return (
-        <div key={product.id}>
-            <h3>{product.productName}</h3>
-            <p>{product.description}</p>
-            <li>{product.price}</li>
-            <li>{product.stock}</li>
-            <img src={product.cloudImage} alt={product.productName} />
+        <div className='w-48 h-96 bg-gray-400 shadow-lg m-4 rounded-md' key={product.id}>
+            <div className='border w-full h-2/3 flex'>
+                <img className='w-2/3 object-contain flex' src={product.cloudImage} alt={product.productName} />
+                <div className='w-1/3 h-full flex flex-col'>
+                    <img className='h-1/3 object-contain mx-2 my-4' src={product.cloudImage} alt={product.productName} />
+                    <img className='h-1/3 object-contain mx-2 my-4' src={product.cloudImage} alt={product.productName} />
+                </div>
+            </div>
+            <div className='border w-full h-1/3 flex flex-col'>
+                <h3>{product.productName}</h3>
+                <p className='text-xs'>{product.description}</p>
+                <li>{product.price}</li>
+                <li>{product.stock}</li>
 
 
-            
-            <button class='border '  onClick={() =>dispatch(props.addToCart(product.id))}>
-                    Add to cart
-            </button>
+                
+                <button class='border '  onClick={() =>dispatch(props.addToCart(product.id))}>
+                        Add to cart
+                </button>
+            </div>
         </div>
     )
 }

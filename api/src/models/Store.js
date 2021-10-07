b61d882821aafb052c05e710a4633185ecfd9c9f
Hernan Curbelo <hernan.cur@hotmail.com>
2021-10-07 10:30:49 -0300

    ANDA

--- api/src/models/Store.js
+++ api/src/models/Store.js
@@ -35,8 +35,8 @@ module.exports = (sequelize) => {
             cp: {
                 type: DataTypes.STRING,
             },
-            cords: {
-                type: DataTypes.STRING,
+            coord: {
+                type: DataTypes.ARRAY(DataTypes.STRING),
             },
             // payment_account: {
             //PENDING

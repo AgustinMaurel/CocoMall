efa0c5749431b2bf8728937a579338107c1d82ee
MatiMonas <matutin97@gmail.com>
2021-10-08 12:26:46 -0300

    adds max results

--- api/src/controllers/Images.js
+++ api/src/controllers/Images.js
@@ -3,8 +3,9 @@ const { cloudinary } = require('../utils/cloudinary');
 const getAllImages = async (req, res) => {
     
     try{
-    const {resources} = await cloudinary.search
+    const {resources} = await cloudinary.search        
         .sort_by('public_id', 'desc')
+        .max_results(500)
         .execute()
 
     const publicIds = resources.map(file => file.public_id)

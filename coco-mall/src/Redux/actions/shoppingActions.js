59795bc491ee5c40521f6d68e430278e58531179
MatiMonas <matutin97@gmail.com>
2021-10-07 23:35:08 -0300

    deletes console log

--- coco-mall/src/Redux/actions/shoppingActions.js
+++ coco-mall/src/Redux/actions/shoppingActions.js
@@ -1,11 +1,15 @@
 import { SHOPPING_CART_TYPES } from './actionTypes';
 
 export function addToCart(id) {
-    console.log(id)
+    
+    //put para actualizar la información del carrito
+
     return { type: SHOPPING_CART_TYPES.ADD_TO_CART, payload: id };
 }
 
 export const deleteFromCart = (id) => {
+
+    
     return { type: SHOPPING_CART_TYPES.REMOVE_ONE_FROM_CART, payload: id };
 };
 export const deleteAllFromCart = (id) => {

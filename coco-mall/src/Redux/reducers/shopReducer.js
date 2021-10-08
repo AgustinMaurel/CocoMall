5098c162d3bf8c41526ad79346fbad5d4658fcd2
MatiMonas <matutin97@gmail.com>
2021-10-08 14:00:49 -0300

    deletes cart state and reducer functions

--- coco-mall/src/Redux/reducers/shopReducer.js
+++ coco-mall/src/Redux/reducers/shopReducer.js
@@ -1,11 +1,10 @@
-import { GET_STORES, SEARCH_BY_NAME, SEARCH_BY_ID, GET_PRODUCT , SHOPPING_CART_TYPES } from '../actions/actionTypes';
+import { GET_STORES, SEARCH_BY_NAME, SEARCH_BY_ID, GET_PRODUCT } from '../actions/actionTypes';
 
 const initialState = {
     allStores: [],
     storesByName: {},
     storeDetail: {},
     storeProducts: [],
-    cart: [],
 };
 
 export const storeReducer = (state = initialState, { type, payload }) => {
@@ -35,46 +34,6 @@ export const storeReducer = (state = initialState, { type, payload }) => {
                 ...state,
                 storeProducts: payload,
             };
-            
-        case SHOPPING_CART_TYPES.ADD_TO_CART: {
-            let newItem = state.storeProducts?.find((item) => item.id === payload);
-            let repeatedItem = state.cart?.find((item) => item.id === newItem.id);
-
-            return repeatedItem
-                ? {
-                      ...state,
-                      cart: state.cart.map((item) =>
-                          item.id === newItem.id ? { ...item, quantity: item.quantity + 1 } : item,
-                      ),
-                  }
-                : { ...state, cart: [...state.cart, { ...newItem, quantity: 1 }] };
-        }
-        case SHOPPING_CART_TYPES.REMOVE_ONE_FROM_CART: {
-            let itemToDelete = state.cart.find((item) => item.id === payload);
-
-            return itemToDelete.quantity > 1
-                ? {
-                      ...state,
-                      cart: state.cart.map((item) =>
-                          item.id === payload ? { ...item, quantity: item.quantity - 1 } : item,
-                      ),
-                  }
-                : { ...state, cart: state.cart.filter((item) => item.id !== payload) };
-        }
-
-        case SHOPPING_CART_TYPES.REMOVE_ALL_FROM_CART: {
-            return {
-                ...state,
-                cart: state.cart.filter((item) => item.id !== payload),
-            };
-        }
-
-        case SHOPPING_CART_TYPES.CLEAR_CART: {
-            return {
-                ...state,
-                cart: []
-            };
-        }
 
         default:
             return state;

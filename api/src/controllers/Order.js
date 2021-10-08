1752b872648d67e51e12434a376afb813a7b0ce1
Daniel Zapata <daniel.zapata.grajales@gmail.com>
2021-10-08 17:13:41 -0500

    little change

--- api/src/controllers/Order.js
+++ api/src/controllers/Order.js
@@ -1,5 +1,5 @@
-const { Order, User , Store, Address} = require('../models/index');
-const ModelController = require('./index');
+const {Order, User, Store, Address} = require("../models/index");
+const ModelController = require("./index");
 
 class OrderModel extends ModelController {
     constructor(model) {
@@ -7,44 +7,39 @@ class OrderModel extends ModelController {
     }
     //Specific Functions for this model
     createOrder = async (req, res, next) => {
-
         const userId = req.body.userId;
         const storeId = req.body.storeId;
         const addressId = req.body.addressId;
 
-
         if (userId && storeId && addressId) {
             try {
-                
                 const order = {
                     amount: req.body.amount,
                     orderState: req.body.orderState,
-                }
-                console.log(userId, storeId, addressId, req.body.amount, req.body.orderState)
-              
+                };
+
                 // create Order
-                const newOrder = await this.model.create(order)
-                console.log(newOrder, '<-----newOrder')
-                
+                const newOrder = await this.model.create(order);
+
                 const orderId = newOrder.id;
                 // add User to order
                 const user = await User.findByPk(userId);
-                await user.addOrder(orderId)
+                await user.addOrder(orderId);
                 // add Store to order
                 const store = await Store.findByPk(storeId);
-                await store.addOrder(orderId)
+                await store.addOrder(orderId);
                 //add Address to order
-                const address = await Address.findByPk(addressId)
-                await address.addOrder(orderId)
+                const address = await Address.findByPk(addressId);
+                await address.addOrder(orderId);
 
-                res.send (newOrder)
+                res.send(newOrder);
             } catch (err) {
-                res.send(err)
+                res.send(err);
             }
         } else {
-            res.status(400).send({ message: 'Wrong parameters'})
+            res.status(400).send({message: "Wrong parameters"});
         }
-    }
+    };
 }
 
 const OrderController = new OrderModel(Order);

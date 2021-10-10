ab22accbea8930b0a9c918e44100af371aaf1b0d
MatiMonas <matutin97@gmail.com>
2021-10-10 19:22:35 -0300

    adds get product images and store images routes

--- api/src/controllers/Images.js
+++ api/src/controllers/Images.js
@@ -1,9 +1,25 @@
 const { cloudinary } = require('../utils/cloudinary');
 
-const getAllImages = async (req, res) => {
+const getStoreImages = async (req, res) => {
     
     try{
-    const {resources} = await cloudinary.search        
+    const {resources} = await cloudinary.search
+    .expression('folder:Stores')        
+        .sort_by('public_id', 'desc')
+        .max_results(500)
+        .execute()
+
+    const publicIds = resources.map(file => file.public_id)
+    res.send(publicIds)
+    }catch(err){
+        console.log(err)
+    }
+}
+const getProductImages = async (req, res) => {
+    
+    try{
+    const {resources} = await cloudinary.search
+    .expression('folder:Product')        
         .sort_by('public_id', 'desc')
         .max_results(500)
         .execute()

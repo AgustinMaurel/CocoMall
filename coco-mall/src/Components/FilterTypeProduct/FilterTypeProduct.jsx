97322379863155db2ece44c6a21c50b4710fd1cc
chris <silvero355@gmail.com>
2021-10-09 21:43:14 -0300

    changes in filter for tests

--- coco-mall/src/Components/FilterTypeProduct/FilterTypeProduct.jsx
+++ coco-mall/src/Components/FilterTypeProduct/FilterTypeProduct.jsx
@@ -2,24 +2,25 @@ import React from 'react';
 
 function FilterTypeProduct({ type, index, handleChecked, check }) {
     return (
-        <button
-            // type='checkbox'
-            // name={type?.id}
-            // value={type?.id}
-            // onChange={(e) => handleChecked(e, index)}
-            // checked={check[index]}
-            className='h-full rounded-lg shadow-md flex flex-col items-center cursor-pointer hover:bg-cocoMall-50 focus:bg-cocoMall-50'
-        >
-            <img className='p-6' src={`/category/${type?.Name}.png`} alt='category' />
-            <label className='text-gray-600 text-sm'>{type?.Name.toUpperCase()}</label>
-            <input 
-                type='checkbox'
-                name={type?.id}
-                value={type?.id}
-                onChange={(e)=> handleChecked(e, index)}
-                checked={check[index]}
-            />
-        </button>
+            <label
+                // type='checkbox'
+                // name={type?.id}
+                // value={type?.id}
+                // onChange={(e) => handleChecked(e, index)}
+                // checked={check[index]}
+                className='h-full rounded-lg shadow-md flex flex-col items-center cursor-pointer hover:bg-cocoMall-50 focus:bg-cocoMall-50'
+            >
+                <img className='p-6' src={`/category/${type?.Name}.png`} alt='category' />
+                <label className='text-gray-600 text-sm'>{type?.Name.toUpperCase()}</label>
+                <input
+                    type='checkbox'
+                    name={type?.id}
+                    value={type?.id}
+                    onChange={(e) => handleChecked(e, index)}
+                    checked={check[index]}
+                    className='checked:bg-cocoMall-400'
+                />
+            </label>
     );
 }
 

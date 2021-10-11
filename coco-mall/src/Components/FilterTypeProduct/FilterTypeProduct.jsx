cc293903f2cef5dcf9b3ae8f9e890fb4b91ecd4a
chris <silvero355@gmail.com>
2021-10-11 16:35:30 -0300

    push for merge

--- coco-mall/src/Components/FilterTypeProduct/FilterTypeProduct.jsx
+++ coco-mall/src/Components/FilterTypeProduct/FilterTypeProduct.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 function FilterTypeProduct({ type, index, handleChecked, check }) {
     let style1 = 'bg-white relative my-4 mx-2 p-2 h-full rounded-lg shadow-md flex flex-col items-center cursor-pointer hover:bg-cocoMall-50';
-    let style2 = 'bg-white relative my-4 mx-2 p-2 h-full rounded-lg shadow-md flex flex-col items-center cursor-pointer bg-cocoMall-50';
+    let style2 = 'relative my-4 mx-2 p-2 h-full rounded-lg shadow-md flex flex-col items-center cursor-pointer bg-cocoMall-50';
 
     return (
-        <label className={check[index] ? style2 : style1}>
+        <label className='bg-white relative my-4 mx-2 p-2 h-full rounded-lg shadow-md flex flex-col items-center cursor-pointer'>
             <div className='h-16 w-16'>
                 <img className='p-2' src={`/category/${type?.Name}.png`} alt='category' />
             </div>
@@ -15,7 +15,7 @@ function FilterTypeProduct({ type, index, handleChecked, check }) {
                 value={type?.id}
                 onChange={(e) => handleChecked(e, index)}
                 checked={check[index]}
-                className='appearance-none absolute top-1 right-1'
+                className='absolute top-2 right-2'
             />
             <label className='text-gray-600 text-sm'>{type?.Name.toUpperCase()}</label>
         </label>

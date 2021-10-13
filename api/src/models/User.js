a62b46240d7b1d007f846e5809825c344d6504d3
Hernan Curbelo <hernan.cur@hotmail.com>
2021-10-13 18:56:01 -0300

    Change dataType

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -47,7 +47,7 @@ module.exports = (sequelize) => {
                 defaultValue: false
             },
             Orders: {
-                type: DataTypes.ARRAY(DataTypes.JSON),
+                type: DataTypes.ARRAY(DataTypes.STRING),
                 defaultValue: []
             }
         },

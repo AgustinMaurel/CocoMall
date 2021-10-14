6e1d3e831d6df8fb2b7edb16775d619b3e3858a2
Daniel Zapata <daniel.zapata.grajales@gmail.com>
2021-10-14 11:31:00 -0500

    changes to Chris

--- api/src/controllers/User.js
+++ api/src/controllers/User.js
@@ -32,7 +32,7 @@ class UserModel extends ModelController {
                     },
                     {
                         model: Address,
-                        attributes: ['address'],
+                        attributes: ['id','address','cords'],
                     },
                 ],
             });

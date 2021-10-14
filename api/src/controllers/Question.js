d73ddcfd2a9447726f5902c03c24c8cf8ab62d23
Hernan Curbelo <hernan.cur@hotmail.com>
2021-10-14 13:51:58 -0300

    put questions

--- api/src/controllers/Question.js
+++ api/src/controllers/Question.js
@@ -2,60 +2,58 @@ const { Question, Product } = require('../models/index');
 const ModelController = require('./index');
 
 class QuestionModel extends ModelController {
-    constructor(model) {
-        super(model);
+  constructor(model) {
+    super(model);
+  }
+  //Specific Functions for this model
+  createQuestion = async (req, res) => {
+    if (req.body.id) {
+      try {
+        //id of Product
+        const { id } = req.body;
+        const { question } = req.body;
+        const obj = {
+          question: req.body.question,
+          answer: 'null',
+        };
+        if (question) {
+          let aux = '';
+          //Create the review
+          const newQuestion = await this.model.create(obj);
+          aux = newQuestion.id;
+          //Search the product and attach the Review
+          const product = await Product.findByPk(id);
+          await product.addQuestion(aux);
+
+          //final Question
+          const finalQuestion = await this.model.findByPk(aux);
+          return res.send(finalQuestion);
+        } else {
+          return res.send('Oops, something went wrong');
+        }
+      } catch (e) {
+        res.send(e);
+      }
+    } else {
+      res.status(400).send({ message: 'Wrong parameters' });
     }
-    //Specific Functions for this model
-    createQuestion = async (req, res) => {
-        if (req.body.id) {
-            try {
-                //id of Product
-                const {id} = req.body;
-                const { question } = req.body
-                const obj = {
-                    question: req.body.question,
-                    answer: "null"
-                }
-                if (question){
-                    let aux = ""
-                    //Create the review
-                    const newQuestion = await this.model.create(obj);
-                    aux = newQuestion.id;
-                    //Search the product and attach the Review
-                    const product = await Product.findByPk(id);
-                    await product.addQuestion(aux);
-                      
-                    //final Question
-                    const finalQuestion = await this.model.findByPk(aux);
-                    return res.send(finalQuestion);
-                }else{
-                    return res.send("Oops, something went wrong")
-                }
-            } catch (e) {
-                res.send(e);
-            }
-        } else {
-            res.status(400).send({ message: 'Wrong parameters' });
-        }
-    };
-    UpdateQuestion = async (req, res) => {
-        if (req.body.id) {
-            try {
-                const finalQuestion = this.model.update(
-                    { answer: req.body.answer },
-                    {
-                        where: {
-                            id: req.body.id,
-                        },
-                    }
-                );
-            } catch (e) {
-                res.send(`error producido:${e}`);
-            }
-        } else {
-            res.send('error falta de argumentos');
-        }
-    };
+  };
+  UpdateQuestion = async (req, res) => {
+    const { id } = req.params;
+    if (id) {
+      try {
+        const finalQuestion = this.model.update(
+          { answer: req.body.answer },
+          { where: { id: id } }
+        );
+        res.send("Answer added successfully")
+      } catch (e) {
+        res.send(e);
+      }
+    } else {
+      res.send('No Question ID provided in params');
+    }
+  };
 }
 
 const QuestionController = new QuestionModel(Question);
--- api/src/routes/Question.js
+++ api/src/routes/Question.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const router = Router();
-const { getAllData, createQuestion } = require('../controllers/Question.js');
+const { getAllData, createQuestion, UpdateQuestion } = require('../controllers/Question.js');
 
 //all routes start with /question
 
 router.get('/', getAllData);
 
 router.post('/create', createQuestion);
+
+router.put('/update/:id', UpdateQuestion)
 
 module.exports = router;

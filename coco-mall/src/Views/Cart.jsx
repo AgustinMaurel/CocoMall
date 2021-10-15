f988025ccca08292ae3e684e5e4d7ad2a747f0e7
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2021-10-15 00:12:54 -0300

    estilo cart mobile

--- coco-mall/src/Views/Cart.jsx
+++ coco-mall/src/Views/Cart.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import NavBar from '../Components/NavBar/NavBar';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
-import { addToCart, deleteAllFromCart, deleteFromCart } from '../Redux/actions/shoppingActions';
+import { addToCart, clearCart, deleteAllFromCart, deleteFromCart } from '../Redux/actions/shoppingActions';
 import { SHOPPING_CART } from '../Scripts/constants';
 import { Image } from 'cloudinary-react'
 
@@ -41,6 +41,9 @@ export default function Cart() {
     const handleAddButton = (id) => {
         dispatch(addToCart(id));
     };
+    const handleClearCart = () => {
+        dispatch(clearCart());
+    };
 
     useEffect(() => {
         return axios.post(SHOPPING_CART.ADD_TO_CART, userCartToBack);
@@ -49,59 +52,65 @@ export default function Cart() {
     function handleCheckout() {
         return userCart.length > 0
             ? axios
-                  .post('/checkout/mercadopago', objectToCheckout)
-                  .then((order) => {
-                      history.push(`/checkout/${order.data.response}`);
-                  })
-                  .catch((err) => console.log(err))
+                .post('/checkout/mercadopago', objectToCheckout)
+                .then((order) => {
+                    history.push(`/checkout/${order.data.response}`);
+                })
+                .catch((err) => console.log(err))
             : false;
     }
 
     return (
-        <div className='h-screen'>
+        <div className='' >
             <div className=' sticky bg-white shadow top-0 z-20'>
                 <NavBar />
             </div>
 
-            <div className='flex flex-col justify-center relative h-full  p-2 '>
-                
-                <div className='flex flex-col relative h-full items-center align-center content-center justify-between p-4 bg-gray-100 '>
-                    
-                    <div className='bg-white rounded flex flex-col shadow-md py-1 h-36 justify-evenly w-full relative'>
-                        <div className='flex flex-row justify-evenly w-full '>
-                            <div className='justify-self-start self-start '>
+            <div className='flex  justify-center relative   py-4 px-1 '>
+            
+                <div className='flex flex-col relative py-5 px-2  w-full h-full items-center align-center content-center justify-around rounded  gap-5  '>
+                <div className='shadow-lg flex items-center self-start justify-center bg-white  border border-red-600  text-primary  rounded h-8 px-1'>
+                            <button onClick={handleClearCart} className='text-xs text-red-600 font-semibold'>Clear Cart</button>
+                        </div>
+                    <div className='bg-white  border border-gray-200 rounded w-full flex flex-col shadow-lg py-5 h-36 justify-around  relative gap-2'>
+
+
+                        <div className='flex flex-row gap-5 justify-around relative w-full px-5'>
+                            <div className='justify-self-start self-start shadow '>
                                 <Image
                                     publicId="Products/xbrogn9dkucn4dptof5j"
-                                    width='125'
+                                    width='100'
                                     className='object-cover flex-none'
                                     cloudName='cocomalls'
                                 />
-                            
+
                             </div>
-                            <div>
-                                <h2 className='font-bolder text-sm'>Skyrim: The elder Scrolls V</h2>
+                            <div className='flex flex-col flex-none flex-no-wrap'>
+                                <h2 className='font-bold text-sm'>Skyrim: The elder Scrolls V</h2>
                                 <p className='text-xs'>Price: 1500</p>
                                 <p className='text-xs'>Stock: 15</p>
                             </div>
                         </div>
 
                         <div className='flex flex-row items-center w-full content-center justify-center gap-5'>
-                            <button 
-                            onClick={handleDeleteOne}
-                            className='h-7 w-7 flex   items-start justify-center bg-secondary  rounded-full text-white font-bold text-lg cursor-pointer'>
+                            <button
+                                onClick={handleDeleteOne}
+                                className='h-7 w-7 flex   items-start justify-center bg-secondary  rounded-full text-white font-bold text-lg cursor-pointer'>
                                 -
                             </button>
-                            <p className='font-bolder'>10</p> 
+                            <p className='font-bolder'>10</p>
 
-                            <button 
-                            onClick={handleAddButton}
-                            className='h-7 w-7 flex  items-start justify-center bg-secondary  rounded-full text-white font-bold text-lg cursor-pointer'>
+                            <button
+                                onClick={handleAddButton}
+                                className='h-7 w-7 flex  items-start justify-center bg-secondary  rounded-full text-white font-bold text-lg cursor-pointer'>
                                 +
                             </button>
                         </div>
                     </div>
+                    <div className='border border-gray-100 w-full'></div>
 
-                    <div className='border border-gray-300 w-full'></div>
+
+
 
                     {/* {userCart.length > 0 ? (
                         userCart.map((el) => (
@@ -142,21 +151,25 @@ export default function Cart() {
                         </div>
                         </> )}*/}
 
-                    {userCart.length > 0 && (
+                    {/* {userCart.length > 0 && ( */}
+
+                    {/* )} */}
+                    {/* <div className='flex w-full justify-between items-center align-middle'> */}
+                        
                         <div
-                            className='shadow-lg flex items-center justify-center bg-white  border border-primary  text-primary w-40 rounded-md h-8             
-                                    xl:border-none xl:shadow-none xl:bg-secondary-light xl:h-12 xl:w-44  '
+                            className='shadow-lg flex items-center justify-center bg-white  border border-primary  text-primary w-40 rounded-md h-8 
+                                        xl:border-none xl:shadow-none xl:bg-secondary-light xl:h-12 xl:w-44  '
                         >
                             <button
                                 className=' focus:outline-none text-center text-base
-                                        md:text-lg
-                                      xl:text-primary xl:text-xl'
+                                            md:text-lg
+                                          xl:text-primary xl:text-xl'
                                 onClick={handleCheckout}
                             >
                                 Go to checkout
                             </button>
-                        </div>
-                    )}
+                        {/* </div> */}
+                    </div>
                 </div>
             </div>
         </div>

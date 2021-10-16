fa27412368e110acdbe6e09d5a6a1872556198e4
Hernan Curbelo <hernan.cur@hotmail.com>
2021-10-16 13:51:52 -0300

    Added creation and delete route for Reviews

--- api/src/routes/Review.js
+++ api/src/routes/Review.js
@@ -1,11 +1,15 @@
 const { Router } = require('express');
 const router = Router();
-const { getAllData, createReview } = require('../controllers/Review.js');
+const { getAllData, createReview, updateReview, deleteReview } = require('../controllers/Review.js');
 
 //all routes start with /review
 router.get('/', getAllData);
 
 router.post('/create', createReview);
+
+router.put('/update/:id', updateReview)
+
+router.delete('/delete/:id', deleteReview)
 
 
 module.exports = router;

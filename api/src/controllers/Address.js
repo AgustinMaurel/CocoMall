f8b05856136e16597818541f52da25b0346322b7
Prym003 <85133493+Prym003@users.noreply.github.com>
2021-10-19 20:31:00 -0300

    address routes fix

--- api/src/controllers/Address.js
+++ api/src/controllers/Address.js
@@ -8,78 +8,63 @@ class AddressModel extends ModelController {
   //Specific Functions for this model
   createAddress = async (req, res) => {
     const { id, directions, cords } = req.body;
+    //id of User
     if (id) {
       try {
-        //id of User
-        const address = {
-          directions,
-          cords: cords
-        };
         //Create the Address
-        const newAddress = await this.model.create(address);
+        const [newAddress, created] = await this.model.findOrCreate({
+          where: {
+            directions: directions,
+            cords: cords
+          }
+        });
         const addressId = newAddress.id;
         //Search the User and attach the Address
         const user = await User.findByPk(id);
         await user.addAddress(addressId);
 
-        const finalAddress = await this.model.findByPk(addressId)
-
-        res.send(finalAddress);
+        res.send(newAddress);
       } catch (e) {
         res.send(e);
       }
     } else {
       res.status(400).send({ message: 'Wrong parameters' });
     }
-    //Specific Functions for this model
-    createAddress = async (req, res) => {
-        const { id, directions, cords } = req.body;
-        if (id) {
-            try {
-                //id of User
+  };
 
-                //Create the Address
-                const [newAddress, created] = await this.model.findOrCreate({
-                    where: {
-                        directions: directions,
-                        cords: cords
-                    }
-                });
-                const addressId = newAddress.id;
-                //Search the User and attach the Address
-                const user = await User.findByPk(id);
-                await user.addAddress(addressId);
-                
-                res.send(newAddress);
-            } catch (e) {
-                res.send(e);
-            }
-        } else {
-            res.status(400).send({ message: 'Wrong parameters' });
-        }
-    };
-    getIdData = async (req, res) => {
-        //lo hice al pedo creo
-        const {id} = req.params
-        if (id) {
-            try {
-                let data = await this.model.findByPk(id);
-                console.log(data)
-                res.send(data);
-            } catch (error) {
-                res.send(error);
-            }
-        } else {
-          res.send('Oops, something went wrong');
-        }
-      } catch (e) {
-        res.send(e);
+  getIdData = async (req, res) => {
+    //lo hice al pedo creo
+    const { id } = req.params
+    if (id) {
+      try {
+        let data = await this.model.findByPk(id);
+        console.log(data)
+        res.send(data);
+      } catch (error) {
+        res.send(error);
       }
     } else {
-      res.json({ error: 'No id in params' });
+      res.send('Oops, something went wrong');
     }
-  };
-}
+  }
+
+  deleteAddress = async (req, res) => {
+    const id = req.params.id
+    if(id){
+      try {
+        await this.model.destroy({
+          where:{
+            id: id
+          }
+        })        
+      } catch (error) {
+        res.send(error)     
+      }
+    } else {
+      res.status(400).send("Wrong params")
+    }
+  }
+};
 
 const AddressController = new AddressModel(Address);
 

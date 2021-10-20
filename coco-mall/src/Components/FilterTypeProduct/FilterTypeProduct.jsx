bcefe6aadf3b16079d789b9b6cbbecc8a36ef748
chris <silvero355@gmail.com>
2021-10-20 16:23:02 -0300

    css filter category

--- coco-mall/src/Components/FilterTypeProduct/FilterTypeProduct.jsx
+++ coco-mall/src/Components/FilterTypeProduct/FilterTypeProduct.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function FilterTypeProduct({ type, index, handleChecked, check, checkType }) {
+    const [isHovered , setIsHovered] = useState(false);
+    const handleMouseEnter = ()=>{
+          setIsHovered(true)
+    }
+    const handleMouseLeave = ()=>{
+          setIsHovered(false)
+    }
+
     return (
         <label
             className={
                 checkType.includes(type?.id)
-                    ? 'bg-white border-4 border-cocoMall-200 relative my-4 mx-2 p-2 h-full rounded-lg shadow-md flex flex-col items-center cursor-pointer hover:shadow-lg hover:border-cocoMall-200'
-                    : 'bg-white border border-transparent relative my-4 mx-2 p-2 h-full rounded-lg shadow-md flex flex-col items-center cursor-pointer hover:shadow-lg hover:border-cocoMall-200'
+                    ? 'bg-white group border-4 border-cocoMall-200 relative my-4 mx-2 p-2 h-full rounded-lg shadow-md flex flex-col items-center cursor-pointer hover:shadow-lg hover:border-cocoMall-200'
+                    : 'bg-white group border border-transparent relative my-4 mx-2 p-2 h-full rounded-lg shadow-md flex flex-col items-center cursor-pointer hover:shadow-lg hover:border-cocoMall-200'
             }
+            onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}
         >
             <div className='h-16 w-16'>
                 <img className='p-2' src={`/category/${type?.Name}.png`} alt='category' />
@@ -20,7 +29,14 @@ function FilterTypeProduct({ type, index, handleChecked, check, checkType }) {
                 checked={check[index]}
                 className='appearance-none absolute top-2 right-2'
             />
-            <label className='text-gray-600 text-sm'>{type?.Name.toUpperCase()}</label>
+            {
+                isHovered
+                ? <p className='text-gray-600 text-sm'>{type?.Name.toUpperCase()}</p>
+                : checkType.includes(type?.id)
+                ? <p className='text-cocoMall-400 text-sm'>{type?.Name.toUpperCase()}</p>
+                : <></>
+            }
+            
         </label>
     );
 }

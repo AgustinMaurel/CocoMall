133a6678a6517af9a014b7b2f3ebeec4f630cd52
chris <silvero355@gmail.com>
2021-10-21 21:11:46 -0300

    review de tienda funcionandoo

--- coco-mall/src/Components/Cards/ReviewCard.jsx
+++ coco-mall/src/Components/Cards/ReviewCard.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import React from 'react';
+import CocoIcon from '../StoreReview/CocoIcon';
 
-function ReviewCard ({username, qualification, description}){
+function ReviewCard({ username, qualification, description }) {
     return (
-        <div className="">
-            <div className="user-info">
-                <div className= "name">
-                    <span>{username}</span>
-                </div>
-                <div className= "qualification">
-                    <span>{qualification}</span>
-                </div>
-            </div>
-            <div className="description">
-                <div>
-                    <span>{description}</span>
-                </div>
+        <div className='bg-white shadow flex flex-col p-10 h-full'>
+            <h3 className='font-bold text-cocoMall-400 text-lg'>{username}</h3>
+            <p className='py-4'>{description}</p>
+
+            <div>
+                {qualification === '1' ? (
+                    <span className='flex'>
+                        <CocoIcon color={'text-cocoMall-500'} />
+                        <CocoIcon />
+                        <CocoIcon />
+                        <CocoIcon />
+                        <CocoIcon />
+                    </span>
+                ) : qualification === '2' ? (
+                    <span>🥥 🥥</span>
+                ) : qualification === '3' ? (
+                    <span>🥥 🥥 🥥</span>
+                ) : qualification === '4' ? (
+                    <span>🥥 🥥 🥥 🥥</span>
+                ) : qualification === '5' ? (
+                    <span>🥥 🥥 🥥 🥥 🥥</span>
+                ) : (
+                    false
+                )}
             </div>
         </div>
-    )
+    );
 }
 
-export default ReviewCard
+export default ReviewCard;

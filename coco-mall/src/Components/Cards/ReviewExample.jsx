6fac3add5928f2ecbfeaf33af53dee1bc4afe835
chris <silvero355@gmail.com>
2021-10-22 10:52:28 -0300

    fix review stars

--- coco-mall/src/Components/Cards/ReviewExample.jsx
+++ coco-mall/src/Components/Cards/ReviewExample.jsx
@@ -3,13 +3,13 @@ import Qualification from '../StoreReview/Qualification';
 
 
 const ReviewExample = () => {
-    let qualification = '1';
+    let qualification = '4';
 
     return (
         <div className='bg-white shadow flex flex-col p-10 h-full'>
             <h3 className='font-bold text-cocoMall-400 text-lg'>Christian Silvero</h3>
             <p className='py-4'>
-                La experiencia fue tremenda! la verdad unos genios! super recomendable.
+                La experiencia fue tremenda! la verdad unos genios!.
             </p>
             <Qualification qualification={qualification} />
         </div>
